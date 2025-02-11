c4f3b29d7be682c4574fb2d159fa21d7d401be99
ahyoon99 <67852906+ahyoon99@users.noreply.github.com>
2025-02-11 22:30:11 +0900

    [Silver V] Title: 다리 놓기, Time: 300 ms, Memory: 20120 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,78 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    // 방법 1
+    // static public void main(String args[]){
+    //     Scanner sc = new Scanner(System.in);
+    //     int t = sc.nextInt();
+    //     for(int testcase=0;testcase<t;testcase++){
+    //         int n = sc.nextInt();
+    //         int m = sc.nextInt();
+    //         BigInteger result = new BigInteger("1");
+
+    //         for(long i=n+1;i<=m;i++){
+    //             BigInteger temp = BigInteger.valueOf(i);
+    //             result = result.multiply(temp);
+    //         }
+
+    //         for(long i=1;i<=(m-n);i++){
+    //             BigInteger temp = BigInteger.valueOf(i);
+    //             result = result.divide(temp);
+    //         }
+            
+    //         System.out.println(result);
+    //     }
+    // }
+
+    // 방법 2 : DP
+    static public void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int[][] result = new int[30][30];
+
+        for(int i=0;i<30;i++){
+            Arrays.fill(result[i],-1);
+        }
+
+        for(int i=0;i<30;i++){
+            result[1][i]=i;
+            result[i][i]=1;
+            result[0][i]=0;
+            result[i][0]=0;
+        }
+
+        
+        // for(int i=0;i<30;i++){
+        //     for(int j=0;j<30;j++){
+        //         System.out.print(result[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }    
+
+        for(int i=0;i<30;i++){
+            for(int j=0;j<30;j++){
+                if(result[i][j]!=-1){
+                    continue;
+                }
+                result[i][j]=result[i-1][j-1]+result[i][j-1];
+            }
+        }
+
+
+        // System.out.println("======================");
+
+        // for(int i=0;i<30;i++){
+        //     for(int j=0;j<30;j++){
+        //         System.out.print(result[i][j]+" ");
+        //     }
+        //     System.out.println();
+        // }        
+        
+        for(int testcase=0;testcase<t;testcase++){ 
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            System.out.println(result[n][m]);
+        }
+    }
+}
