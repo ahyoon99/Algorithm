ea9d384c52a9f7829fbacb8662ca8e6dda7be78b
ahyoon99 <67852906+ahyoon99@users.noreply.github.com>
2026-01-06 21:58:27 +0900

    [Silver II] Title: DFS와 BFS, Time: 264 ms, Memory: 24364 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -2,103 +2,82 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n;
-    static int m;
-    static int v;;
-    static ArrayList<Integer>[] matrix;
-    //static int[][] matrix;
-    
-
-    static public void main(String args[]) throws IOException{
-        //System.setIn(new FileInputStream("res/input.txt"));
-        input();
-
-        // for(int i=0;i<n+1;i++){
-
-        // System.out.println(matrix[i].toString());
-        // }
-
-        dfs(v);
-        System.out.println();
-        bfs(v);
-    }
-
-    static void dfs(int v){
-        Stack<Integer> st = new Stack<Integer>();
-        boolean[] visited = new boolean[n+1];
-
-        st.add(v);
-        visited[v]=true;
-        boolean flag;
-        System.out.print(v+" ");
-
-        while(!st.isEmpty()){
-            int num = st.peek();
-            flag = false;
-            ArrayList<Integer> temp = matrix[num];
-            for(int i=0;i<temp.size();i++){
-                if(!visited[temp.get(i)]){
-                    st.push(temp.get(i));
-                    visited[temp.get(i)]=true;
-                    System.out.print(temp.get(i)+" ");
-                    flag=true;
-                    break;
-                }
-            }
-
-            if(!flag){
-                st.pop();
-            }
-        }
-    }
-
-    static void bfs(int v){
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n+1];
-
-        q.add(v);
-        visited[v]=true;
-
-        while(!q.isEmpty()){
-            int num = q.poll();
-            System.out.print(num+" ");
-            ArrayList<Integer> temp = matrix[num];
-            for(int i=0;i<temp.size();i++){
-                int next = temp.get(i);
-                if(!visited[next]){
-                    q.add(next);
-                    visited[next]=true;
-                }
-            }
-        }
-    }
-
-    static void input() throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        v = Integer.parseInt(st.nextToken());
-        
-        matrix = new ArrayList[n+1];
-        for(int i=0;i<=n;i++){
-            matrix[i] = new ArrayList<Integer>();
-        }
-
-        for(int i=0;i<m;i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            matrix[start].add(end);
-            matrix[end].add(start);
-            
-        }
-
-        for (int i=0; i<n+1; i++) {
-			Collections.sort(matrix[i]);
+	static int n;
+	static int m;
+	static int v;
+	
+	static int[][] matrix;
+	
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		input();
+		
+		boolean[] visited = new boolean[n];
+		dfs(v-1, visited);
+		
+		sb.append("\n");
+		
+		bfs();
+		
+		System.out.println(sb.toString());
+	}
+	
+	static void dfs(int num, boolean[] visited) {
+		if(visited[num]) {
+			return;
 		}
-    }
+		sb.append((num+1)+" ");
+		visited[num]=true;
+		
+		for(int i=0;i<n;i++) {
+			if(matrix[num][i]==1 && visited[i]==false) {
+				dfs(i, visited);
+			}
+		}
+	}
+	
+	static void bfs() {
+		Queue<Integer> q = new LinkedList<>();
+		boolean[] visited = new boolean[n];
+		
+		visited[v-1]=true;
+		q.add(v-1);
+		sb.append((v)+" ");
+		
+		while(!q.isEmpty()) {
+			int num = q.poll();
+			for(int i=0;i<n;i++) {
+				if(matrix[num][i]==1 && visited[i]==false) {
+					q.add(i);
+					visited[i]=true;
+					sb.append((i+1)+" ");
+				}
+			}
+		}
+		
+	}
+	
+	static void input() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		sb = new StringBuilder();
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		v = Integer.parseInt(st.nextToken());
+		
+		matrix = new int[n][n];
+		
+		for(int i=0;i<m;i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int start = Integer.parseInt(st.nextToken())-1;
+			int end = Integer.parseInt(st.nextToken())-1;
+			matrix[start][end]=1;
+			matrix[end][start]=1;
+		}
+	}
+
 }
