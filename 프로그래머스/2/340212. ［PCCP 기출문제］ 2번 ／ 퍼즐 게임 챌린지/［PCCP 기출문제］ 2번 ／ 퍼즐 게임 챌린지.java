ebb78b2646616522074d99fa3eceeff16272e0f8
ahyoon99 <67852906+ahyoon99@users.noreply.github.com>
2026-03-24 10:53:25 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 30.62 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -2,42 +2,69 @@ import java.util.*;
 
 class Solution {
     static int n;
+    static int[] diffs;
+    static int[] times;
+    static long limit;
     static int answer;
+    
     public int solution(int[] diffs, int[] times, long limit) {
-        answer = Integer.MAX_VALUE;
-
+        
         n = diffs.length;
-
-        int start=1;
-        int end = 100000;
-        while(start<=end){
-            int mid = (start+end)/2;
-            if(calculate(mid, diffs, times, limit)){
-                answer = Math.min(answer, mid);
-                end = mid-1;
-            }
-            else{
-                start=mid+1;
-            }
-        }
+        this.diffs = diffs;
+        this.times = times;
+        this.limit = limit;
+        
+        
+        answer = Integer.MAX_VALUE;
+        
+        bs(1, 100000);
+        
         return answer;
     }
-
-    static boolean calculate(int level, int[] diffs, int[] times, long limit){
-        long total=0;
-        for(int i=0;i<n;i++){
-            if(diffs[i]<=level){
-                total+=times[i];
-            }
-            else{
-                int loop = diffs[i]-level;
-                total+=(times[i]+times[i-1])*loop+times[i];
+    
+    public void bs(int start, int end){
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(calculate(mid)){
+                answer = Math.min(answer, mid);
+                end = mid-1;
+            }else{
+                start = mid+1;
             }
         }
-        if(total<=limit){
-            answer = level;
-            return true;
+    }
+    
+    
+    
+    public boolean calculate(int level){
+        if(level<1){
+            return false;
         }
-        return false;
+        
+        long totalTime = times[0];
+        int prevTime = times[0];
+        
+        for(int i=1;i<n;i++){
+            // 계산 도중, 소요시간이 limit보다 커지면 바로 false 리턴 
+            if(totalTime>limit){
+                return false;
+            }
+            int diff = diffs[i];
+            int curTime = times[i];
+            
+            if(diff<=level){
+                totalTime += curTime;
+            }else if(diff>level){
+                int loop = diff-level;
+                totalTime+=loop*(prevTime+curTime);
+                totalTime+=curTime;
+            }
+            prevTime = curTime;
+        }
+        
+        if(totalTime>limit){
+            return false;
+        }
+        return true;
     }
 }
