39ac62a33578b7d709b8e0d300ad30e1cdd9e830
ahyoon99 <67852906+ahyoon99@users.noreply.github.com>
2026-08-05 21:56:35 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.61 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,15 +1,15 @@
 import java.util.*;
 
 class Solution {
+    static int n;
+    static int m;
+    
+    static int[][] maps;
+    static int[][] distance;
     
     static int[] dx = {-1,0,1,0};
     static int[] dy = {0,1,0,-1};
     
-    static int n;
-    static int m;
-    
-    static int[][] distance;
-
     static class Node{
         int x;
         int y;
@@ -20,44 +20,51 @@ class Solution {
     }
     
     public int solution(int[][] maps) {
+        int answer = 0;
+        
         n = maps.length;
         m = maps[0].length;
+        
+        this.maps = maps;        
+        
+        bfs(0, 0);
+        answer = distance[n-1][m-1];        
+        
+        return answer;
+    }
+    
+    public void bfs(int x, int y){
         distance = new int[n][m];
+        
         for(int i=0;i<n;i++){
             Arrays.fill(distance[i], -1);
         }
         
-        bfs(maps);
+        Queue<Node> q = new LinkedList<>();
         
-        return distance[n-1][m-1];
-    }
-    
-    public void bfs(int[][] maps){
-        Queue<Node> q =  new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
-        q.add(new Node(0,0));
-        distance[0][0]=1;
+        q.add(new Node(x, y));
+        distance[x][y]=1;
         
         while(!q.isEmpty()){
             Node node = q.poll();
-            int x = node.x;
-            int y = node.y;
+            x = node.x;
+            y = node.y;
+            
             for(int i=0;i<4;i++){
                 int nx = x+dx[i];
                 int ny = y+dy[i];
                 if(!isValid(nx, ny)){
                     continue;
                 }
-                if(visited[nx][ny]==false && maps[nx][ny]==1){
-                    q.add(new Node(nx, ny));
-                    visited[nx][ny]=true;
+                if(maps[nx][ny]==1 && distance[nx][ny]==-1){
                     distance[nx][ny]=distance[x][y]+1;
+                    q.add(new Node(nx, ny));
                 }
             }
         }
     }
     
-    public boolean isValid(int x, int y){
+    boolean isValid(int x, int y){
         if(0<=x && x<n && 0<=y && y<m){
             return true;
         }
